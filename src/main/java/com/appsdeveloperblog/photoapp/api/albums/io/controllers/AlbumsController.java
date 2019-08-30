7e0c9147374ab6f7876d92738f0e75260d109533
Alexandr Shevtsov <ashevtsov@luxoft.com>
2019-08-30 13:47:37 +0300

    refactoring

--- src/main/java/com/appsdeveloperblog/photoapp/api/albums/io/controllers/AlbumsController.java
+++ src/main/java/com/appsdeveloperblog/photoapp/api/albums/io/controllers/AlbumsController.java
@@ -5,49 +5,38 @@
  */
 package com.appsdeveloperblog.photoapp.api.albums.io.controllers;
 
-import com.appsdeveloperblog.photoapp.api.albums.data.AlbumEntity;
 import com.appsdeveloperblog.photoapp.api.albums.service.AlbumsService;
 import com.appsdeveloperblog.photoapp.api.albums.ui.model.AlbumResponseModel;
-import java.util.ArrayList;
-import java.util.List;
 import org.modelmapper.ModelMapper;
-import java.lang.reflect.Type;
-import org.modelmapper.TypeToken;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
 @RestController
 @RequestMapping("/users/{id}/albums")
 public class AlbumsController {
-    
-    @Autowired
-    AlbumsService albumsService;
-    Logger logger = LoggerFactory.getLogger(this.getClass());
-    @GetMapping( 
-            produces = { 
-                MediaType.APPLICATION_JSON_VALUE,
-                MediaType.APPLICATION_XML_VALUE,
-            })
-    public List<AlbumResponseModel> userAlbums(@PathVariable String id) {
 
-        List<AlbumResponseModel> returnValue = new ArrayList<>();
-        
-        List<AlbumEntity> albumsEntities = albumsService.getAlbums(id);
-        
-        if(albumsEntities == null || albumsEntities.isEmpty())
-        {
-            return returnValue;
-        }
-        
-        Type listType = new TypeToken<List<AlbumResponseModel>>(){}.getType();
- 
-        returnValue = new ModelMapper().map(albumsEntities, listType);
-        logger.info("Returning " + returnValue.size() + " albums");
-        return returnValue;
+    private final AlbumsService albumsService;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    public AlbumsController(AlbumsService albumsService) {
+        this.albumsService = albumsService;
+    }
+
+    @GetMapping
+    public List<AlbumResponseModel> userAlbums(@PathVariable String id) {
+        ModelMapper mapper = new ModelMapper();
+        List<AlbumResponseModel> userAlbums = albumsService.getAlbums(id).stream()
+                .map(albumEntity -> mapper.map(albumEntity, AlbumResponseModel.class))
+                .collect(toList());
+        logger.info("Returning " + userAlbums.size() + " albums");
+        return userAlbums;
     }
 }
